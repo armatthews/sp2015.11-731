20cde281f95e68b78db7fd1efd3334deed2df4ef
Austin Matthews <austinma@cs.cmu.edu>
2015-01-19 10:12:55 -0500

    i think it's working

--- hw1/test.py
+++ hw1/test.py
@@ -7,7 +7,7 @@ if False:
 	start_probs = numpy.array([0.6, 0.4])
 	transition_probs = numpy.array([[0.7, 0.3], [0.4, 0.6]])
 	emission_probs = numpy.array([[0.1, 0.4, 0.5], [0.6, 0.3, 0.1]])
-elif False:
+elif True:
 	states = ('Healthy', 'Fever')
 	observations = ('normal', 'cold', 'dizzy')
 	start_probs = numpy.array([0.6, 0.4])
@@ -19,7 +19,7 @@ elif False:
 	start_probs = numpy.array([0.5, 0.5])
 	transition_probs = numpy.array([[0.7, 0.3], [0.3, 0.7]])
 	emission_probs = numpy.array([[0.9, 0.1], [0.2, 0.8]])
-elif True:
+elif False:
 	states = ('rain', 'no rain')
 	observations = ('umbrella', 'no umbrella')
 	start_probs = numpy.array([0.5, 0.5])
@@ -85,78 +85,40 @@ def xi(forward_probs, backward_probs, transition_probs, emission_probs, observat
 				x[t][i][j] = forward_probs[t][i] * transition_probs[i][j] * backward_probs[t + 1][j] * emission_probs[j][observations[t]] / z
 	return x
 
-	print 'x:'
-	print x.shape
-	print x
-	print
-
-	fp = forward_probs[:len(observations) - 1,:, numpy.newaxis]
+def xi_v(forward_probs, backward_probs, transition_probs, emission_probs, observations):
+	fp = forward_probs[:len(observations),:, numpy.newaxis]
 	tp = transition_probs[numpy.newaxis,:,:]
-	"""print 'fp:'
-	print fp.shape
-	print forward_probs[:len(observations) - 1, :]
-	print
-	print 'tp:'
-	print tp.shape
-	print tp
-	print
-	#print 'dot:'
-	#print fp.dot(tp)
-	#print
-	print 'bc:'
-	print (fp * tp).shape
-	print fp * tp
-	print"""
-	bp = backward_probs[1:len(observations),numpy.newaxis,:]
-	"""print 'bp:'
-	print bp.shape
-	print bp
-	print"""
-	"""print 'bc:'
-	print (fp * tp * bp).shape
-	print fp * tp * bp"""
-	ep = numpy.array([emission_probs.transpose()[observations[t]] for t in range(1, len(observations))])[:,numpy.newaxis,:]
-	"""print 'ep:'
-	print ep.shape
-	print ep
-	print
-	print 'bc:'
-	print (fp * tp * bp * ep).shape
-	print (fp * tp * bp * ep)"""
-	z = numpy.array([1.0/(forward_probs[t].dot(backward_probs[t])) for t in range(0, len(observations) - 1)])[:,numpy.newaxis,numpy.newaxis]
-	"""print 'z:'
-	print z.shape
-	print z
-	print"""
-	print 'bc:'
-	print (fp * tp * bp * ep * z).shape
-	print (fp * tp * bp * ep * z)
-	print
-	sys.exit()
-	return x
+	bp = backward_probs[1:len(observations) + 1,numpy.newaxis,:]
+	ep = numpy.array([emission_probs.transpose()[observations[t]] for t in range(0, len(observations))])[:,numpy.newaxis,:]
+	z = numpy.array([1.0/(forward_probs[t].dot(backward_probs[t])) for t in range(0, len(observations))])[:,numpy.newaxis,numpy.newaxis]
+	return (fp * tp * bp * ep * z)
 
 def viterbi(start_probs, transition_probs, emission_probs, observations):
 	V = numpy.zeros((len(observations), N))
-	B = numpy.zeros((len(observations), N))
+	B = [[0 for i in range(N)] for j in range(len(observations))]
 	V[0] = start_probs * emission_probs.transpose()[observations[0]]
-	print V[0]
+	
 	for t in range(1, len(observations)):
 		for i in range(N):
 			k = max(range(N), key=lambda k: transition_probs[k][i] * V[t - 1][k])
 			V[t][i] = emission_probs[i][observations[t]] * transition_probs[k][i] * V[t - 1][k]
 			B[t][i] = k
 
-	print V
-	print B
+	Xf = max(range(N), key=lambda k: V[len(observations) - 1][k])
+	X = [Xf]
+	for t in range(len(observations) - 1, 0, -1):
+		X.append(B[t][X[-1]])
+	X = X[::-1]
+	return X
 
-observations = [0, 0, 1, 0, 0]
+observations = [2, 1, 0]
 viterbi(start_probs, transition_probs, emission_probs, observations)
-
+sys.exit()
 for iteration in range(1000):
 	forward_probs = forward(start_probs, transition_probs, emission_probs, observations)
 	backward_probs = backward(transition_probs, emission_probs, observations)
 	g = gamma(forward_probs, backward_probs)
-	x = xi(forward_probs, backward_probs, transition_probs, emission_probs, observations)
+	x = xi_v(forward_probs, backward_probs, transition_probs, emission_probs, observations)
 	if False:
 		print forward_probs
 		print
@@ -178,8 +140,7 @@ for iteration in range(1000):
 		T = numpy.zeros(N)
 		for j in range(N):
 			t = sum(x[t][i][j] for t in range(0, len(observations)))
-			assert abs(s) > 1.0e-100
-			T[j] = t / s
+			T[j] = t / s if abs(s) > 1.0e-100 else transition_probs[i][j]
 		T = normalize(T)
 		transition_probs[i] = T
 
@@ -190,14 +151,13 @@ for iteration in range(1000):
 		s = sum(g[t][i] for t in range(1, len(observations) + 1))
 		for k in range(K):
 			t = sum(g[t + 1][i] for t in range(len(observations)) if observations[t] == k)
-			T[k] = t / s
+			T[k] = t / s if abs(s) > 1.0e-100 else emission_probs[i][k]
 		emission_probs[i] = T
 
 	# Compute new start probs
-	T = normalize(numpy.linalg.inv(transition_probs).dot(g[1]))
-	start_probs = T
+	#T = normalize(numpy.linalg.inv(transition_probs).dot(g[1]))
+	#start_probs = T
 
 print start_probs
 print transition_probs
 print emission_probs
-print gamma(forward_probs, backward_probs)
