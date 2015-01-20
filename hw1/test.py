527dd361b63147b479668ad2a35b3464ceb26363
Austin Matthews <austinma@cs.cmu.edu>
2015-01-19 22:49:59 -0500

    mostly working

--- hw1/test.py
+++ hw1/test.py
@@ -17,11 +17,11 @@ class HiddenMarkovModel:
 
 	# forward[t][i] = p(X_i, y_1:i | theta) 
 	def forward(self, observations):
-		state_probs = self.start_probs
-		forward_probs = [state_probs]
+		forward_probs = []
 		for i in range(len(observations)):
+			state_probs = state_probs.dot(self.transition_probs) if i > 0 else self.start_probs
 			obs_matrix = numpy.diag(self.emission_probs.transpose()[observations[i]])
-			state_probs = state_probs.dot(self.transition_probs).dot(obs_matrix)
+			state_probs = state_probs.dot(obs_matrix)
 			forward_probs.append(state_probs)
 		return numpy.array(forward_probs)
 
@@ -29,7 +29,7 @@ class HiddenMarkovModel:
 	def backward(self, observations):
 		state_probs = numpy.array([1.0 for state in states])
 		backward_probs = [state_probs]
-		for i in range(len(observations))[::-1]:
+		for i in range(1,len(observations))[::-1]:
 			obs_matrix = numpy.diag(self.emission_probs.transpose()[observations[i]])
 			state_probs = self.transition_probs.dot(obs_matrix).dot(state_probs)
 			backward_probs.append(state_probs)
@@ -37,6 +37,7 @@ class HiddenMarkovModel:
 
 	# gamma[t][i] = p(X_t = i | Y, theta)
 	def gamma(self, forward_probs, backward_probs):
+		assert forward_probs.shape == backward_probs.shape
 		g = []
 		for f, b in zip(forward_probs, backward_probs):
 			p = normalize(f * b)
@@ -45,12 +46,33 @@ class HiddenMarkovModel:
 
 	# xi[t][i][j] = p(X_t = i, X_t+1 = j | Y, theta)
 	def xi(self, forward_probs, backward_probs, observations):
-		fp = forward_probs[:len(observations),:, numpy.newaxis]
+		fp = forward_probs[:-1, :, numpy.newaxis]
 		tp = self.transition_probs[numpy.newaxis,:,:]
-		bp = backward_probs[1:len(observations) + 1,numpy.newaxis,:]
-		ep = numpy.array([self.emission_probs.transpose()[observations[t]] for t in range(0, len(observations))])[:,numpy.newaxis,:]
-		z = numpy.array([1.0/(forward_probs[t].dot(backward_probs[t])) for t in range(0, len(observations))])[:,numpy.newaxis,numpy.newaxis]
-		return (fp * tp * bp * ep * z)
+		bp = backward_probs[1:,numpy.newaxis,:]
+		ep = numpy.array([self.emission_probs.transpose()[observations[t]] for t in range(0, len(observations))])[1:,numpy.newaxis,:]
+		z = numpy.array([1.0/(forward_probs[t].dot(backward_probs[t])) for t in range(0, len(observations))])[:-1,numpy.newaxis,numpy.newaxis]
+		"""print forward_probs[0][1]
+		print transition_probs[1][0]
+		print emission_probs[0][1]
+		print backward_probs[1][0]
+		print"""
+
+		x = fp * tp * bp * ep * z
+		return x
+
+	def old_xi(self, forward_probs, backward_probs, observations):
+		#xi[0,1,0] should be 0.02102447163 = 0.075 * 0.1 * 0.6 * 0.2561 / 0.05544
+		x = numpy.zeros((len(observations) - 1, self.N, self.N))
+		y = forward_probs.dot(self.transition_probs[:,:])
+		z = 0.0
+		for t in range(len(observations)-1):
+			z = forward_probs[t][:].dot(backward_probs[t][:])
+			assert abs(z) > 1.0e-100
+			for i in range(self.N):
+				for j in range(self.N):
+					x[t][i][j] = forward_probs[t][i] * self.transition_probs[i][j] * backward_probs[t + 1][j] * self.emission_probs[j][observations[t + 1]] / z
+		return x
+
 
 	# Finds argmax_X of p(X | Y, theta)
 	def viterbi(self, observations):
@@ -76,36 +98,55 @@ class HiddenMarkovModel:
 		backward_probs = self.backward(observations)
 		g = self.gamma(forward_probs, backward_probs)
 		x = self.xi(forward_probs, backward_probs, observations)
+		"""print 'forward:'
+		print forward_probs
+		print 'backward:'
+		print backward_probs
+		print 'gamma:'
+		print g
+		print 'xi:'
+		print x"""
+		#sys.exit()
 
 		# Compute new transition probs
 		for i in range(self.N):
 			# s is the expected number of times we pass through state i 
 			s = sum(g[t][i] for t in range(0, len(observations)))
-			T = numpy.zeros(self.N)
-			for j in range(self.N):
-				t = sum(x[t][i][j] for t in range(0, len(observations)))
-				T[j] = t / s if abs(s) > 1.0e-100 else self.transition_probs[i][j]
-			T = normalize(T)
-			self.transition_probs[i] = T
+			assert s >= 0.0
+			if s > 1.0e-100 and len(observations) > 1:
+				T = numpy.zeros(self.N)
+				for j in range(self.N):
+					# t is the expected number of times we transition from state i to state j
+					t = sum(x[t][i][j] for t in range(0, len(observations) - 1))
+					T[j] = t / s
+					T = normalize(T)
+				self.transition_probs[i] = T
+			else:
+				# if s is 0, we never pass through this state.
+				# in this case, the values of the parameters don't matter,
+				# so we just leave them at what they were
+				pass
 
 		# Compute new emission probs
 		for i in range(self.N):
-			T = numpy.zeros(self.K)
 			# s is the expected number of times we pass through state i
-			s = sum(g[t][i] for t in range(1, len(observations) + 1))
-			for k in range(self.K):
-				t = sum(g[t + 1][i] for t in range(len(observations)) if observations[t] == k)
-				T[k] = t / s if abs(s) > 1.0e-100 else self.emission_probs[i][k]
-			self.emission_probs[i] = T
+			s = sum(g[t][i] for t in range(0, len(observations)))
+			assert s >= 0.0
+			if s > 0:
+				T = numpy.zeros(self.K)
+				for k in range(self.K):
+					t = sum(g[t][i] for t in range(len(observations)) if observations[t] == k)
+					T[k] = t / s
+				T = normalize(T)
+				self.emission_probs[i] = T
 
 		# Compute new start probs
-		T = normalize(numpy.linalg.inv(self.transition_probs).dot(g[1]))
-		#self.start_probs = T
+		self.start_probs = g[0]
 
 		obs_prob = sum(forward_probs[-1])
 		return obs_prob
 
-if False:
+if True:
 	states = ('Rainy', 'Sunny')
 	observations = ('walk', 'shop', 'clean')
 	start_probs = numpy.array([0.6, 0.4])
@@ -117,7 +158,7 @@ elif False:
 	start_probs = numpy.array([0.6, 0.4])
 	transition_probs = numpy.array([[0.7, 0.3], [0.4, 0.6]])
 	emission_probs = numpy.array([[0.5, 0.4, 0.1], [0.1, 0.3, 0.6]])
-elif True:
+elif False:
 	states = ('rain', 'no rain')
 	observations = ('umbrella', 'no umbrella')
 	start_probs = numpy.array([0.5, 0.5])
@@ -127,24 +168,35 @@ elif False:
 	states = ('rain', 'no rain')
 	observations = ('umbrella', 'no umbrella')
 	start_probs = numpy.array([0.5, 0.5])
-	transition_probs = numpy.array([[0.6667, 0.3333], [1.0, 0.0]])
-	emission_probs = numpy.array([[1.0, 0.0], [0.0, 1.0]])
+	transition_probs = numpy.array([[0.6667, 0.3333], [0.99, 0.01]])
+	emission_probs = numpy.array([[0.99, 0.01], [0.01, 0.99]])
 elif False:
 	states = ('state 1', 'state 2')
 	observations = ('no eggs', 'eggs')
 	start_probs = numpy.array([0.2, 0.8])
 	transition_probs = numpy.array([[0.5, 0.5], [0.3, 0.7]])
 	emission_probs = numpy.array([[0.3, 0.7], [0.8, 0.2]])
-else:
+elif False:
 	states = ('1', '2')
 	observations = ('1')
 	start_probs = numpy.array([1.0, 0.0])
 	transition_probs = numpy.array([[0.6, 0.4], [0.1234, 0.8766]])
 	emission_probs = numpy.array([[1.0], [1.0]])
+else:
+	states = ('s', 't')
+	observations = ('A', 'B')
+	start_probs = numpy.array([0.85, 0.15])
+	transition_probs = numpy.array([[0.3, 0.7], [0.1, 0.9]])
+	emission_probs = numpy.array([[0.4, 0.6], [0.5, 0.5]])
 
 hmm = HiddenMarkovModel(states, observations, start_probs, transition_probs, emission_probs)
-observations = [0, 0, 1, 0, 0]
+observations = [0, 1, 2]
 print hmm.viterbi(observations)
+
+a = hmm.forward(observations)
+b = hmm.backward(observations)
+x = hmm.xi(a, b, observations)
+old_x = hmm.old_xi(a, b, observations)
 
 for iteration in range(1000):
 	obs_prob = hmm.update_parameters(observations)
@@ -153,4 +205,5 @@ for iteration in range(1000):
 print hmm.start_probs
 print hmm.transition_probs
 print hmm.emission_probs
-print numpy.linalg.inv(hmm.transition_probs)
+print hmm.viterbi(observations)
+
